61d5a4d9dbbb83a146df5d40cd44b434a2020163
Gecko93 <nicolas.lpfan@gmail.com>
2024-08-27 11:06:02 -0300

    Creación de la rest API

---
+++ src/controller.js
@@ -0,0 +1,31 @@
+import {pool} from './database.js';
+
+class LibroController{
+	
+	async getAll(req,res) {
+		const result = await pool.query('SELECT * FROM libros');
+		res.json(result);
+	}
+
+	async add(req, res){
+		const libro = req.body;
+		const [result] = await pool.query(`INSERT INTO libros(nombre, autor, categoria, anio_publicacion, ISBN) VALUES (?, ?, ?, ?, ?)`,
+		[libro.nombre, libro.autor, libro.categoria, libro.anio_publicacion, libro.ISBN]);
+		res.json({"Id insertado": result.insertId});
+	}
+
+	async delete(req, res){
+		const libro = req.body;
+		const [result] = await pool.query(`DELETE FROM libros WHERE ISBN=(?)`,[libro.ISBN]);
+		res.json({"Registros eliminados": result.affectedRows});
+	}
+
+	async update(req, res){
+		const libro = req.body;
+		const [result] = await pool.query(`UPDATE libros SET nombre=(?), autor=(?), categoria=(?), anio_publicacion=(?), ISBN=(?) WHERE id=(?)`,[libro.nombre, libro.autor, libro.categoria, libro.anio_publicacion, libro.ISBN, libro.id]);
+		res.json({"Registros actualizados": result.changedRows});
+	}
+
+}
+
+export const libro = new LibroController();
